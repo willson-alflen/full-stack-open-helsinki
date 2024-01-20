f6d346d992acf128239aaa4f33f1f0906faa540e
willson-alflen <wilson.alflen@gmail.com>
2024-01-20 08:51:45 -0300

    create part1 directory + complete exercise 1.1

---
+++ part1/courseinfo/src/components/Content/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Content({ part, exercises }) {
+  return (
+    <div>
+      <p>
+        {part} {exercises}
+      </p>
+    </div>
+  )
+}
+
+export default Content
---
+++ part1/courseinfo/src/components/Total/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Total({ numberOfExercises }) {
+  return (
+    <div>
+      <p>Number of exercises {numberOfExercises}</p>
+    </div>
+  )
+}
+
+export default Total
