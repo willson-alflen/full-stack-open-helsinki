1ee0165eecc7e2067815f4b68bf0eb00924cdffe
willson-alflen <wilson.alflen@gmail.com>
2024-01-22 15:12:44 -0300

    complete exercise 1.9

--- part1/unicafe/src/components/Statistics/index.tsx
+++ part1/unicafe/src/components/Statistics/index.tsx
@@ -4,12 +4,18 @@ export default function Statistics(props) {
   return (
     <section>
       <h2>Statistics</h2>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.all}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.positive} %</p>
+      {props.all ? (
+        <>
+          <p>good {props.good}</p>
+          <p>neutral {props.neutral}</p>
+          <p>bad {props.bad}</p>
+          <p>all {props.all}</p>
+          <p>average {props.average}</p>
+          <p>positive {props.positive} %</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </section>
   )
 }
